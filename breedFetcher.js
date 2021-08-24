671c1a796f0145c98050af5cf69879be3353ec6e
Arushi Katyal <katy.arushi@gmail.com>
2021-08-24 02:43:15 -0400

    complete assigmment

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,17 @@
 const request = require('request');
-const breed = process.argv[2]
+const breed = process.argv[2];
 
-const url = 'https://api.thecatapi.com/v1/breeds/search?q='
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breed;
 
 request(url, (error, response, body) => {
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  const data = JSON.parse(body);
-  //console.log(data);
-  console.log(data[0].description);
+  if (error) {
+    console.log(`The following error occured: ${error}`);
+  } else {
+    const data = JSON.parse(body);
+    if ((data[0] !== undefined) && (response.statusCode === 200)) {
+      console.log(data[0].description);
+    } else {
+      console.log("Sorry, the breed you requested has not been found.");
+    }
+  }
 });
